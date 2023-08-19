620252944e001e114c6e7653cbbcceec5fa1d936
Guilherme Gomes <gui.rock08@gmail.com>
2023-08-19 17:32:06 -0300

    Adding a start to counter

--- src/renderer.js
+++ src/renderer.js
@@ -2,14 +2,19 @@ function sleep(msTime) {
     return new Promise(resolve => setTimeout(resolve, msTime))
 }
 
-async function counter() {
-    let count = 1
+async function counter(count) {
     let countdown = document.getElementById("seconds")
-    while(count <= 10) {
+    while(count <= 25) {
         countdown.textContent = count
         await sleep(1000)
         count++
     }
 }
 
-counter()
+(function startTimer() {
+    let count = 1
+    let buttonStart = document.getElementById("start")
+    buttonStart.onclick = () => {
+        counter(count)
+    }
+}())
