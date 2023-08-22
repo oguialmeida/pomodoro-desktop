8f4b0cb82edbd05beee1730cfca5fb824acf744f
Guilherme Gomes <gui.rock08@gmail.com>
2023-08-22 13:08:16 -0300

    Adding productive time functionality

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,5 @@
 let count = 1
+let totalCount = 1
 let countdownInterval
 
 function pause() {
@@ -13,32 +14,55 @@ function reset() {
     document.querySelector("body").style.backgroundColor = "#0085ff"
 }
 
+function productiveReset() {
+    clearInterval(countdownInterval)
+    totalCount = 1
+    document.getElementById("productive").textContent = "00:00:00"
+}
+
+function formatTime(seconds, formatType = false) {
+    const dateObj = new Date(seconds * 1000)
+    const hours = dateObj.getUTCHours().toString().padStart(2, '0')
+    const minutes = dateObj.getUTCMinutes().toString().padStart(2, '0')
+    const secs = dateObj.getUTCSeconds().toString().padStart(2, '0')
+
+    if (formatType === true) {
+        return `${minutes}:${secs}`
+    }
+    return `${hours}:${minutes}:${secs}`
+}
+
 function counter() {
     let countdown = document.getElementById("seconds")
     let over = document.getElementById("over")
+    let productive = document.getElementById("productive")
     let background = document.querySelector("body")
     let trocar = false
-    
+
     countdownInterval = setInterval(() => {
-        let dateObj = new Date(count * 1000)
-        let minutes = dateObj.getMinutes().toString().padStart(2, '0')
-        let seconds = dateObj.getSeconds().toString().padStart(2, '0')
-
-        countdown.textContent = `${minutes}:${seconds}`
-
-        if(count < 1500) {
+        if (count < 1500) {
             count++
-        } else if(count === 1500) {
+
+        } else if (count === 1500) {
             count = 1
             over.textContent = "Let's take a break :)"
             background.style.backgroundColor = "#38BDAE"
             trocar = true
         }
-        if(trocar && count === 300) {
+
+        if (trocar === false) {
+            totalCount++
+        }
+
+        if (trocar && count === 300) {
             reset()
             counter()
         }
-    }, 1000)
+
+        countdown.textContent = formatTime(count, true)
+        productive.textContent = formatTime(totalCount)
+
+    }, 10)
 }
 
 (function startTimer() {
@@ -47,23 +71,24 @@ function counter() {
     let buttonReset = document.getElementById("reset")
 
     buttonStart.onclick = () => {
-        buttonStart.disabled=true
-        buttonPause.disabled=false
-        buttonReset.disabled=false
+        buttonStart.disabled = true
+        buttonPause.disabled = false
+        buttonReset.disabled = false
         counter()
     }
 
     buttonPause.onclick = () => {
-        buttonStart.disabled=false
-        buttonPause.disabled=true
-        buttonReset.disabled=false
+        buttonStart.disabled = false
+        buttonPause.disabled = true
+        buttonReset.disabled = false
         pause()
     }
 
     buttonReset.onclick = () => {
-        buttonStart.disabled=false
-        buttonPause.disabled=true
-        buttonReset.disabled=true
+        buttonStart.disabled = false
+        buttonPause.disabled = true
+        buttonReset.disabled = true
         reset()
+        productiveReset()
     }
-}())
+})()
